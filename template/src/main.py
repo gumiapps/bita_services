deac70b7e155aa5f3483a065f4ee3f50503dfb78
kaleb wondimu <kalebwondimu33@gmail.com>
2024-12-04 19:02:58 +0300

    guideline and template for services

---
+++ template/src/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+# Create an instance of FastAPI
+app = FastAPI()
+
+# Define a route for the root URL
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
